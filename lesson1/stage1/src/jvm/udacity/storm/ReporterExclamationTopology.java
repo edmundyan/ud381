47d3f5e2f5aef5d934f913143c2802b321fd002d
Edmund Yan <edmund.yan@venmo.com>
2016-05-05 17:32:53 -0400

    lessons1/stage1

--- lesson1/stage1/src/jvm/udacity/storm/ReporterExclamationTopology.java
+++ lesson1/stage1/src/jvm/udacity/storm/ReporterExclamationTopology.java
@@ -27,6 +27,8 @@ import java.util.Map;
 //</dependency>
 //
 //********* END 1-of-4
+import com.lambdaworks.redis.RedisClient;
+import com.lambdaworks.redis.RedisConnection;
 
 /**
  * This is a basic example of a Storm topology.
@@ -54,6 +56,7 @@ public class ReporterExclamationTopology {
     //********* TO DO 2-of-4
     // place holder to keep the connection to redis
 
+    RedisConnection<String,String> redis;
 
     //********* END 2-of-4
 
@@ -68,6 +71,9 @@ public class ReporterExclamationTopology {
 
       //********* TO DO 3-of-4
       // instantiate a redis connection
+      RedisClient client = new RedisClient("localhost", 6379);
+      redis = client.connect();
+
 
       // initiate the actual connection
 
@@ -88,8 +94,8 @@ public class ReporterExclamationTopology {
       _collector.emit(tuple, new Values(exclamatedWord.toString()));
 
       //********* TO DO 4-of-4 Uncomment redis reporter
-      //long count = 30;
-      //redis.publish("WordCountTopology", exclamatedWord.toString() + "|" + Long.toString(count));
+      long count = 30;
+      redis.publish("WordCountTopology", exclamatedWord.toString() + "|" + Long.toString(count));
       //********* END 4-of-4
     }
 
